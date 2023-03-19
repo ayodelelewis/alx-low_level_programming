b8a0316a089d1a41e000e87b03d958298851c63f
Ayodele Lewis <ayodelelewis@outlook.com>
2023-03-19 04:30:08 -0700

    Hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - entry point of the program
+ * main -entry point of the program
  *
- * Return: Always 0 (Success)
+ * Return:| 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
